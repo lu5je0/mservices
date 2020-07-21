56c648135784d35853ae5942d40d39e7947ce2bf
lijunjie <lijunjie01@souche.com>
2020-07-21 15:42:58 +0800

    init

---
+++ storage/src/test/java/com/lu5je0/storage/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lu5je0.storage.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
